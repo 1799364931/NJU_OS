2d6cffb1b6a8fc0335925ccebe93278fc13391ca
tracer-nju <tracer@nju.edu.cn>
2024-11-21 20:35:25 +0800

    Linux LAPTOP-NHUBKET1 5.15.167.4-microsoft-standard-WSL2 #1 SMP Tue Nov 5 00:21:55 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  20:35:25 up  1:52,  1 user,  load average: 0.10, 0.13, 0.10

--- M2/.shadow/libco/co.c
+++ M2/.shadow/libco/co.c
@@ -60,8 +60,8 @@ static inline void
 stack_switch_call(void *sp, void *entry, __uint64_t arg) {
     asm volatile (
 #if __x86_64__
-        "movq %0, %%rsp"
-        "movq %2, %%rdi"
+        "movq %0, %rsp"
+        "movq %2, %rdi"
         "jmp *%1"
           :
           : "b"((__uint64_t)sp),
