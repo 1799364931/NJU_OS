ba250e518822ed652d6350293e63266c3c7575d6
tracer-nju <tracer@nju.edu.cn>
2024-11-22 13:33:23 +0800

    Linux LAPTOP-NHUBKET1 5.15.167.4-microsoft-standard-WSL2 #1 SMP Tue Nov 5 00:21:55 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  13:33:23 up 20 min,  1 user,  load average: 0.11, 0.13, 0.09

--- M2/.shadow/libco/co.c
+++ M2/.shadow/libco/co.c
@@ -70,14 +70,14 @@ stack_switch_call(/*void *sp,*/ void *entry, void* arg) {
             "r"(arg)
           : "memory"
 #else
-        "movl %0, %%esp\n\t"
+   /*     "movl %0, %%esp\n\t"
         "movl %2, 4(%0)\n\t"
         "jmp *%1\n\t"
           :
           : "b"((__uint32_t)sp - 8),
             "d"(entry),
             "a"(arg)
-          : "memory"
+          : "memory" */
 #endif
     );
 }
