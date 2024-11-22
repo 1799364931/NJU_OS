8de203fb5b8519bc61ac906609677825963577f4
tracer-nju <tracer@nju.edu.cn>
2024-11-23 00:07:01 +0800

    Linux LAPTOP-NHUBKET1 5.15.167.4-microsoft-standard-WSL2 #1 SMP Tue Nov 5 00:21:55 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  00:07:01 up 2 min,  1 user,  load average: 0.29, 0.19, 0.08

--- M2/.shadow/libco/tests/main.c
+++ M2/.shadow/libco/tests/main.c
@@ -29,12 +29,12 @@ static void work_loop(void *arg) {
 }
 
 static void work(void *arg) {
-    printf("%d",5);
+   
     work_loop(arg);
 }
 
 static void test_1() {
-       
+        printf("%d",5);
     struct co *thd1 = co_start("thread-1", work, "X");
     struct co *thd2 = co_start("thread-2", work, "Y");
 
