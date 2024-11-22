c6761a2a15f4cc2d485cc4bc2790721517b0e5ba
tracer-nju <tracer@nju.edu.cn>
2024-11-22 19:18:41 +0800

    Linux LAPTOP-NHUBKET1 5.15.167.4-microsoft-standard-WSL2 #1 SMP Tue Nov 5 00:21:55 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  19:18:41 up  1:00,  1 user,  load average: 0.43, 0.25, 0.18

--- M2/.shadow/libco/co.c
+++ M2/.shadow/libco/co.c
@@ -43,7 +43,7 @@ struct co *co_start(const char *name, void (*func)(void *), void *arg) {
     new_co->arg=arg;
     new_co->status=CO_NEW;
     co_list[length_co_list++]=new_co;
-    //co_yield();
+    co_yield();
 
     return new_co;
 }
