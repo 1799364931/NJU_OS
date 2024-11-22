0b6f72d50919bfc1c04364bee4481404c254c644
tracer-nju <tracer@nju.edu.cn>
2024-11-22 13:32:45 +0800

    Linux LAPTOP-NHUBKET1 5.15.167.4-microsoft-standard-WSL2 #1 SMP Tue Nov 5 00:21:55 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  13:32:45 up 20 min,  1 user,  load average: 0.12, 0.14, 0.10

--- M2/.shadow/libco/co.c
+++ M2/.shadow/libco/co.c
@@ -58,14 +58,14 @@ void co_wait(struct co *co) {
 
 
 static inline void
-stack_switch_call(void *sp, void *entry, void* arg) {
+stack_switch_call(/*void *sp,*/ void *entry, void* arg) {
     asm volatile (
 #if __x86_64__
-        "movq %0,%%rsp\n\t"
+       // "movq %0,%%rsp\n\t"
         "movq %2,%%rdi\n\t"
         "jmp *%1\n\t"
           :
-          : "r"(sp),
+          : //"r"(sp),
             "r"(entry),
             "r"(arg)
           : "memory"
@@ -99,7 +99,7 @@ void co_yield() {
                 "movq %%rsp,%0"
                 :"=r"(sp)
             );
-            stack_switch_call(sp,next_co->func,next_co->arg);
+            stack_switch_call(next_co->func,next_co->arg);
         }
         else if(next_co->status==CO_WAITING){
             
