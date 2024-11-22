afa8fe8ec5e6edd28661efb9062d2bcbc973b527
tracer-nju <tracer@nju.edu.cn>
2024-11-22 22:17:31 +0800

    Linux LAPTOP-NHUBKET1 5.15.167.4-microsoft-standard-WSL2 #1 SMP Tue Nov 5 00:21:55 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  22:17:31 up  3:59,  1 user,  load average: 1.34, 1.11, 0.73

--- M2/.shadow/libco/co.c
+++ M2/.shadow/libco/co.c
@@ -101,13 +101,13 @@ void co_yield() {
     if(val==0){
         //保存现场的
         //随机选择一个切换
-        printf("%s\n",current_co->name);
+        //printf("%s\n",current_co->name);
         struct co* next_co=co_list[rand()%(length_co_list)];
         
         current_co=next_co;
         
         if(next_co->status==CO_RUNNING){
-            printf("\n hehe \n");
+            //printf("\n hehe \n");
             longjmp(next_co->context,1);
         }
         else if(next_co->status==CO_NEW){
