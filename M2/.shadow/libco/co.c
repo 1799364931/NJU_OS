e686f66f3e5fb7fa8599d56bc1155326468d7ebd
tracer-nju <tracer@nju.edu.cn>
2024-11-22 22:58:24 +0800

    Linux LAPTOP-NHUBKET1 5.15.167.4-microsoft-standard-WSL2 #1 SMP Tue Nov 5 00:21:55 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  22:58:24 up  4:29,  1 user,  load average: 0.77, 1.16, 1.06

--- M2/.shadow/libco/co.c
+++ M2/.shadow/libco/co.c
@@ -61,8 +61,10 @@ void co_wait(struct co *co) {
     //如果当前协程调用了 wait，那就让当前协程进行等待
     current_co->status=CO_WAITING;
     co->waiter=co;
-    while(co->status!=CO_DEAD) // 如果进程co没结束，就一直等待
-
+    while(co->status!=CO_DEAD){
+        co_yield();
+    } // 如果进程co没结束，就一直等待
+    
     length_co_list--;
     free(co);
     co=NULL;
