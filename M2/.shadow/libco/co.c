74c6541b7faeea9ae83e3bdc601be179ee290233
tracer-nju <tracer@nju.edu.cn>
2024-11-22 13:33:35 +0800

    Linux LAPTOP-NHUBKET1 5.15.167.4-microsoft-standard-WSL2 #1 SMP Tue Nov 5 00:21:55 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  13:33:35 up 20 min,  1 user,  load average: 0.09, 0.13, 0.09

--- M2/.shadow/libco/co.c
+++ M2/.shadow/libco/co.c
@@ -62,8 +62,8 @@ stack_switch_call(/*void *sp,*/ void *entry, void* arg) {
     asm volatile (
 #if __x86_64__
        // "movq %0,%%rsp\n\t"
-        "movq %2,%%rdi\n\t"
-        "jmp *%1\n\t"
+        "movq %1,%%rdi\n\t"
+        "jmp *%0\n\t"
           :
           : //"r"(sp),
             "r"(entry),
