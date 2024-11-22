de2c13a56143a6a2e697de327e3043f953d596c4
tracer-nju <tracer@nju.edu.cn>
2024-11-22 19:49:58 +0800

    Linux LAPTOP-NHUBKET1 5.15.167.4-microsoft-standard-WSL2 #1 SMP Tue Nov 5 00:21:55 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  19:49:58 up  1:31,  1 user,  load average: 0.16, 0.09, 0.12

--- M2/.shadow/libco/tests/main.c
+++ M2/.shadow/libco/tests/main.c
@@ -16,15 +16,16 @@ static int get_count() {
 
 static void work_loop(void *arg) {
     const char *s = (const char*)arg;
-    //printf("test");
+    
     for (int i = 0; i < 100; ++i) {
-        //printf("%s%d  ", s, get_count());
+        printf("%s%d  ", s, get_count());
         add_count();
         co_yield();
     }
 }
 
 static void work(void *arg) {
+    printf("test");
     work_loop(arg);
 }
 
