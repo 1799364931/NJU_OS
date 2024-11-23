63fd092ba7fe42f5bc85342ca6182843852c8b05
tracer-nju <tracer@nju.edu.cn>
2024-11-23 11:06:20 +0800

    Linux LAPTOP-NHUBKET1 5.15.167.4-microsoft-standard-WSL2 #1 SMP Tue Nov 5 00:21:55 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  11:06:20 up 47 min,  1 user,  load average: 0.27, 0.18, 0.15

--- M2/.shadow/libco/tests/main.c
+++ M2/.shadow/libco/tests/main.c
@@ -37,10 +37,10 @@ static void test_1() {
       //只要进去了 就不能使用这个抽象的怪异函数printf
     work("X");
     //struct co *thd1 = co_start("thread-1", work, "X");
-    struct co *thd2 = co_start("thread-2", work, "Y");
+    //struct co *thd2 = co_start("thread-2", work, "Y");
 
    // co_wait(thd1);
-    co_wait(thd2);
+    //co_wait(thd2);
 
 //    printf("\n");
 }
