4a644f9af6c3207e06d4b8f45dfd41ea49e04e74
tracer-nju <tracer@nju.edu.cn>
2024-11-23 11:41:47 +0800

    Linux LAPTOP-NHUBKET1 5.15.167.4-microsoft-standard-WSL2 #1 SMP Tue Nov 5 00:21:55 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  11:41:47 up  1:22,  1 user,  load average: 0.10, 0.14, 0.18

--- M2/.shadow/libco/tests/main.c
+++ M2/.shadow/libco/tests/main.c
@@ -17,7 +17,7 @@ static int get_count() {
 static void work_loop(void *arg) {
     const char *s = (const char*)arg;
     
-    for (int i = 0; i < 100; ++i) {
+    for (int i = 0; i < 10; ++i) {
 
         printf("%s%d  ", s, get_count());
     
