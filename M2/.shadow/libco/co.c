cde85b029853ab29c5c2b347902fe4dc24804c98
tracer-nju <tracer@nju.edu.cn>
2024-11-23 21:14:25 +0800

    Linux LAPTOP-NHUBKET1 5.15.167.4-microsoft-standard-WSL2 #1 SMP Tue Nov 5 00:21:55 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  21:14:25 up  8:28,  1 user,  load average: 0.48, 0.29, 0.30

--- M2/.shadow/libco/co.c
+++ M2/.shadow/libco/co.c
@@ -69,8 +69,8 @@ void co_wait(struct co *co) {
         co_yield();
     } // 如果进程co没结束，就一直等待
     current_co->status=CO_RUNNING;
-    // free(co->name);
-    // free(co);
+    free(co->name);
+    free(co);
     //进程co结束，释放资源
     return;
 }
